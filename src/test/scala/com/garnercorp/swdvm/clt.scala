5d8500b14ecd968fb7db1f6015b8f84117ac89e3
Piergiuliano Bossi <pgbossi@gmail.com>
2016-09-14 19:08:48 -0400

    Clarify the problem with not considering partial sums

--- src/test/scala/com/garnercorp/swdvm/clt.scala
+++ src/test/scala/com/garnercorp/swdvm/clt.scala
@@ -4,6 +4,7 @@ import Sample._
 
 object CLT extends App {
 
+  println(s"Non randomized values: ${EstimateSample.Values.map(task => Elapsed(task.estimate * 1.1 / 2.0))}")
   val random = RandomVariation.ChiSquaredWithSeed0
   val runs = Vector.tabulate(1000)(_ => EstimateSample.Values.map(task => Elapsed(task.estimate *
                                                                                   1.1 /
@@ -28,5 +29,49 @@ object CLT extends App {
   val datesDistroWithDates = datesDistro.map(m => new Percentiles(m.percentiles.values.mapValues(time => new
       DateTime((time * 60 * 1000).toLong))))
 
-  println(datesDistroWithDates.map(_.asString))
+  println(datesDistroWithDates.map(_.asString).mkString("\n=>"))
+  println("\n\nUp to here is the old spike, showing the distribution on the calendar and how more spread it is compared to")
+  println("what runs calculate. What follows is a simplified demo.\n\n")
+  
+  val rand = new util.Random(0)
+  val taskNum = 1000
+  val iterationsNum = 50
+  
+  val iterations = Vector.tabulate(iterationsNum)(_ => Vector.tabulate(taskNum)(_ => 1.0 + rand.nextInt(10)))
+  
+  def distros(in: Vector[Vector[Double]]): Vector[MonteCarlo[Elapsed]] = in.map(vector => new MonteCarlo(vector.map(e => Elapsed(e))))
+  def singleDistro(in: Vector[Double]): MonteCarlo[Elapsed] = new MonteCarlo(in.map(e => Elapsed(e)))
+  def partialSums(in: Vector[Double]): Vector[Double] = Vector.tabulate(in.size)(i => in.slice(0, i+1).sum)
+  
+  println(s"iterationRuns = ${iterations.mkString("\n")}")
+  println(s"iterationRuns distros = ${distros(iterations).mkString("\n")}")
+  val durations = iterations.map(it => it.sum)
+  println(s"durations = $durations")
+  println(s"durations distro = ${singleDistro(durations)}")
+  println(s"avg duration = ${durations.sum/durations.size}")
+  
+  val partialDurations = iterations.map(partialSums(_))
+  println(s"partialDurations = ${partialDurations.mkString("\n")}")
+  println(s"partialDurations distros = ${distros(partialDurations).mkString("\n")}")
+
+  val taskDurations = partialDurations.transpose
+  println(s"taskDurations = ${taskDurations.mkString("\n")}")
+  println(s"taskDurations distros = ${distros(taskDurations).mkString("\n")}")
+  
+  val projectCompletion = taskDurations.last
+  println(s"projectCompletion = $projectCompletion")
+  println(s"projectCompletion distro = ${singleDistro(projectCompletion)}")
+  
+  val taskAverages = iterations.transpose.map(samples => samples.sum/samples.size)
+  println(s"taskAverages = $taskAverages")
+  println(s"taskAverages distro = ${singleDistro(taskAverages)}")
+  
+  val wrongProjectCompletion = iterations.transpose.last.map(_ + distros(taskDurations)(taskDurations.size-2).avgTime)
+  println(s"wrongProjectCompletion = $wrongProjectCompletion")
+  println(s"wrongProjectCompletion distro = ${singleDistro(wrongProjectCompletion)}")
+  
+  println("\n\nprojectCompletion has the same avg as wrongProjectCompletion but a much bigger stddev (3x+), because of the")
+  println("Central Limit Theorem => if we start from an average we have lost all the variability that the stddev captures,")
+  println("we have to keep the whole distribution instead. Note that the bigger taskNum the bigger the error.")
+  println("With 1000 tasks it's 2.93 vs 81.18 (with the latter being the accurate value)!!")
 }
