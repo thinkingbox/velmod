0941fcdd59ff6bd68d1ca18234a168e63a5c2714
Giuliano Bossi <pgbossi@gmail.com>
2016-12-28 14:15:05 -0500

    Reduce test name redundancy

--- src/test/scala/com/garnercorp/swdvm/Strategy1UTest.scala
+++ src/test/scala/com/garnercorp/swdvm/Strategy1UTest.scala
@@ -4,15 +4,15 @@ import org.scalatest.{Matchers, FunSuite}
 
 class Strategy1UTest extends FunSuite with Matchers {
   val Strategy = Strategy1(Task.empty, 3, 6.0)
-  test("strategy1: time, work actual & velocity are calculated directly from work required & programmers") {
+  test("time, work actual & velocity are calculated directly from work required & programmers") {
     Strategy.time shouldBe 2.0
     Strategy.workActual shouldBe 6.0
     Strategy.velocity shouldBe 3
   }
-  test("strategy1: there is never accumulation of tech debt") {
+  test("there is never accumulation of tech debt") {
     Strategy.techDebt shouldBe 0.0
   }
-  test("strategy1: run ignores previous tasks and takes a snapshot with factor and random variation") {
+  test("run ignores previous tasks and takes a snapshot with factor and random variation") {
     val ignoredPreviousTaskRun = TaskRun(-42, 42)
     val task = Task(20)
     val Programmers = 2000
