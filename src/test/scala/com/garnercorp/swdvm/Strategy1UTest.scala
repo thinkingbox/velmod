2704aa79f06b6dd67de943adaf7bc0ae06030b0a
Giuliano Bossi <pgbossi@gmail.com>
2016-12-28 08:47:46 -0500

    Add more tests for Strategy1

--- src/test/scala/com/garnercorp/swdvm/Strategy1UTest.scala
+++ src/test/scala/com/garnercorp/swdvm/Strategy1UTest.scala
@@ -3,12 +3,20 @@ package com.garnercorp.swdvm
 import org.scalatest.{Matchers, FunSuite}
 
 class Strategy1UTest extends FunSuite with Matchers {
-  test("strategy1: given work required & programmers calculate time, work actual, tech debt & velocity") {
-    val taskRun = Strategy1(Task.empty, 3, 6.0)
-    taskRun.programmers shouldBe 3
-    taskRun.time shouldBe 2.0
-    taskRun.workActual shouldBe 6.0
-    taskRun.techDebt shouldBe 0.0
-    taskRun.velocity shouldBe 3
+  val Strategy = Strategy1(Task.empty, 3, 6.0)
+  test("strategy1: time, work actual & velocity are calculated directly from work required & programmers") {
+    Strategy.time shouldBe 2.0
+    Strategy.workActual shouldBe 6.0
+    Strategy.velocity shouldBe 3
+  }
+  test("strategy1: there is never accumulation of tech debt") {
+    Strategy.techDebt shouldBe 0.0
+  }
+  test("strategy1: run ignores previous tasks and takes a snapshot with factor and random variation") {
+    val ignoredPreviousTaskRun = TaskRun(-42, 42)
+    val task = Task(20)
+    val Programmers = 2000
+    Strategy1.run(ignoredPreviousTaskRun, task)(Programmers, 3, RandomVariation.always(0.2)) shouldBe
+      Strategy1(task, Programmers, 12)
   }
 }
