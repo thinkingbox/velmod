0a51e03d66e6b2b4fa0d84f237769d020d41848b
Giuliano Bossi <pgbossi@gmail.com>
2016-12-28 21:02:57 -0500

    Reduce test name redundancy and add test for previous velocity 0

--- src/test/scala/com/garnercorp/swdvm/Strategy2UTest.scala
+++ src/test/scala/com/garnercorp/swdvm/Strategy2UTest.scala
@@ -4,7 +4,19 @@ import org.scalatest.mock.EasyMockSugar
 import org.scalatest.{FunSuite, Matchers}
 
 class Strategy2UTest extends FunSuite with Matchers with EasyMockSugar {
-  test("strategy2: given a previous task run with a certain velocity & tech debt, " +
+  test("it takes an infinite time if previous velocity is 0 and no other quantity can be calculated") {
+    val taskRun = Strategy2(TaskRun(0, 0),
+      Task.empty,
+      desiredTime = 5.0,
+      workRequired = 12.0,
+      programmers = 2,
+      deathTechDebt = 100.0)
+    taskRun.time shouldBe Double.PositiveInfinity
+    taskRun.workActual.isNaN shouldBe true
+    taskRun.techDebt.isNaN shouldBe true
+    taskRun.velocity.isNaN shouldBe true
+  }
+  test("given a previous task run with a certain velocity & tech debt, " +
       "time, work required, programmers and death tech debt, " +
       "calculate work actual, tech debt & velocity") {
     val taskRun = Strategy2(TaskRun(1.5, 7),
@@ -19,7 +31,7 @@ class Strategy2UTest extends FunSuite with Matchers with EasyMockSugar {
     taskRun.techDebt shouldBe 11.5
     taskRun.velocity shouldBe 1.77
   }
-  test("strategy2: when time > work required / velocity and we are on time or early, " +
+  test("when time > work required / velocity and we are on time or early, " +
        "tech debt reduces with an efficiency factor") {
     val taskRun = Strategy2(TaskRun(1.5, 7),
                             Task.empty,
@@ -34,7 +46,7 @@ class Strategy2UTest extends FunSuite with Matchers with EasyMockSugar {
     taskRun.velocity shouldBe 1.905
     taskRun.balance shouldBe 0
   }
-  test("strategy2: when time > work required / velocity, but there's no tech debt, " +
+  test("when time > work required / velocity, but there's no tech debt, " +
        "then work actual = work required and balance is positive") {
     val taskRun = Strategy2(TaskRun(2, 0),
                             Task.empty,
@@ -48,7 +60,7 @@ class Strategy2UTest extends FunSuite with Matchers with EasyMockSugar {
     taskRun.velocity shouldBe 2
     taskRun.balance shouldBe 4
   }
-  test("strategy2: when time > work required / velocity and we are late, " +
+  test("when time > work required / velocity and we are late, " +
       "time saved goes on being more on time first, rather than recovering tech debt") {
     val taskRun = Strategy2(TaskRun(1.5, 7, -2),
                             Task.empty,
@@ -63,7 +75,7 @@ class Strategy2UTest extends FunSuite with Matchers with EasyMockSugar {
     taskRun.velocity shouldBe 1.8825
     taskRun.balance shouldBe 0
   }
-  test("strategy2: tech debt cannot become negative and velocity cannot increase the ideal value (# of programmers)") {
+  test("tech debt cannot become negative and velocity cannot increase the ideal value (# of programmers)") {
     val taskRun = Strategy2(TaskRun(2, 1),
                             Task.empty,
                             desiredTime = 10.0,
@@ -76,7 +88,7 @@ class Strategy2UTest extends FunSuite with Matchers with EasyMockSugar {
     taskRun.techDebt shouldBe 0
     taskRun.velocity shouldBe 2
   }
-  test("strategy2: actual work cannot be less than 25% of work required") {
+  test("actual work cannot be less than 25% of work required") {
     val taskRun = Strategy2(TaskRun(2, 0),
                             Task.empty,
                             desiredTime = 1.0,
@@ -88,7 +100,7 @@ class Strategy2UTest extends FunSuite with Matchers with EasyMockSugar {
     taskRun.techDebt shouldBe 7.5
     taskRun.velocity shouldBe 1.85
   }
-  test("strategy2: velocity cannot become negative") {
+  test("velocity cannot become negative") {
     val taskRun = Strategy2(TaskRun(2, 0),
                             Task.empty,
                             desiredTime = 1.0,
@@ -100,12 +112,12 @@ class Strategy2UTest extends FunSuite with Matchers with EasyMockSugar {
     taskRun.techDebt shouldBe 750
     taskRun.velocity shouldBe 0
   }
-  test("when running strategy2, if previous run reaches velocity 0 the next run is dead") {
+  test("when running, if previous run reaches velocity 0 the next run is dead") {
     val next = Strategy2.run(Map.empty[Task, Double], 100)(TaskRun(0, 100), Task.empty)(2, 1, RandomVariation.always(1))
     next.velocity shouldBe 0
     next.time shouldBe 0
   }
-  test("when running strategy2, next run uses snapshot data from previous run, without recalculating") {
+  test("when running, next run uses snapshot data from previous run, without recalculating") {
     val previous = mock[TaskRun]
     previous.snapshot.andReturn(TaskRun(2, 50)).anyTimes()
     previous.velocity.andReturn(1).times(1).andThrow(new RuntimeException).anyTimes()
@@ -118,7 +130,7 @@ class Strategy2UTest extends FunSuite with Matchers with EasyMockSugar {
       next.techDebt shouldBe 52
     }
   }
-  test("strategy2: if we are early with the previous task and the next one requires more work than desired, " +
+  test("if we are early with the previous task and the next one requires more work than desired, " +
       "reduce balance but tech debt is unaffected if balance still positive") {
     val taskRun1 = Strategy2(TaskRun(2, 0),
                              Task.empty,
@@ -147,7 +159,7 @@ class Strategy2UTest extends FunSuite with Matchers with EasyMockSugar {
     taskRun3.workActual shouldBe 54
     taskRun3.techDebt shouldBe 0
   }
-  test("strategy2: even if we were early before, constantly underestimating will eventually lead to tech debt, " +
+  test("even if we were early before, constantly underestimating will eventually lead to tech debt, " +
        "consuming all balance and affecting velocity") {
     val taskRun1 = Strategy2(TaskRun(2, 0),
                              Task.empty,
@@ -171,7 +183,7 @@ class Strategy2UTest extends FunSuite with Matchers with EasyMockSugar {
     taskRun2.techDebt shouldBe 10.2 +- 0.01
     taskRun2.velocity shouldBe 1.796 +- 0.01
   }
-  test("strategy2: when balance is 0 if next task is desired to be done in less than 25% of time, " +
+  test("when balance is 0 if next task is desired to be done in less than 25% of time, " +
        "balance goes negative and task is late") {
     val taskRun3 = Strategy2(TaskRun(1.796, 10.2),
                              Task.empty,
@@ -185,7 +197,7 @@ class Strategy2UTest extends FunSuite with Matchers with EasyMockSugar {
     taskRun3.techDebt shouldBe 131.7
     taskRun3.velocity shouldBe 0
   }
-  test("strategy2: even when balance is > 0 if next task is desired to be done in less than 25% of time, " +
+  test("even when balance is > 0 if next task is desired to be done in less than 25% of time, " +
        "balance can go negative and task is late") {
     val taskRun2 = Strategy2(TaskRun(2, 0, 10.1),
                              Task.empty,
