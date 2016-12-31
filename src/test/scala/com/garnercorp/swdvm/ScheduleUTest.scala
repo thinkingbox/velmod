1a0162afa23944d35428cbd4592919b6358a88d0
Giuliano Bossi <pgbossi@gmail.com>
2016-12-31 08:38:32 -0500

    More test name change to clarify what's going on

--- src/test/scala/com/garnercorp/swdvm/ScheduleUTest.scala
+++ src/test/scala/com/garnercorp/swdvm/ScheduleUTest.scala
@@ -6,14 +6,14 @@ import com.github.nscala_time.time.Imports._
 
 class ScheduleUTest extends FunSuite with Matchers with Dates {
   val Start = date(2015, 8, 20)
-  test("when there are no tasks delivery is immediate") {
+  test("when there are no tasks end date is immediate") {
     Schedule(Start, CompositeElapsed()).end shouldBe Start
   }
-  test("map task deliveries based on a starting date") {
+  test("map end date based on starting date and individual times") {
     val projectRun = CompositeElapsed(Elapsed(1.0), Elapsed(2.0))
     Schedule(Start, projectRun).end shouldBe Start + 3.days
   }
-  test("delivery maps task runs in order from a starting date") {
+  test("delivery maps elapsed times in order from a starting date") {
     val projectRun = CompositeElapsed(Elapsed(0.5), Elapsed(2.0), Elapsed(3.0))
     Schedule(Start, projectRun).delivery shouldBe Vector(
       ScheduleEntry(dateTime(2015, 8, 20, 12), Elapsed(0.5)),
